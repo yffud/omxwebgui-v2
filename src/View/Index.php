72b89848b69128d4ea7ee34308d2d8b135a2e1d8
Shane Duffy <shane.duffy@hyh.com>
2018-06-20 15:12:46 -0700

    Annotation interface

--- src/View/Index.php
+++ src/View/Index.php
@@ -198,7 +198,7 @@ class Index extends View
                 <div class="time"></div>
 
             </div>
-            <div class="video-annotation"><div class="btn btn-success" onclick="$('.annotation').toggleClass('hidden'); setAnnotationTime();">Add Annotation</div></div>
+            <div class="video-annotation"><div class="btn btn-success" data-key="65" onclick="$('.annotation').toggleClass('hidden'); setAnnotationTime();">Add Annotation</div></div>
             <div class="annotation hidden">
 <!--                <div class="annotation-time"></div>
                 <div class="annotation-time-store"></div>
